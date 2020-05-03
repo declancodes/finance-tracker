fed4c435d738a07d070bd58c45bfabba681ae707
DeclanCodes <DeclanCodes@users.noreply.github.com>
2020-05-03 11:52:59 -0400

    update non-applied holding entity migration: - Remove UNIQUE name constraint to allow for accounts to have the same fund - Add UNIQUE (account_uuid, ticker_symbol) constraint to prohibit having the same holding in the same account

--- flyway/sql/V3__Add_holding.sql
+++ flyway/sql/V3__Add_holding.sql
@@ -1,7 +1,8 @@
 CREATE TABLE holding (
     holding_uuid UUID PRIMARY KEY,
     account_uuid UUID NOT NULL REFERENCES account ON DELETE RESTRICT,
-    name VARCHAR(255) UNIQUE NOT NULL,
-    ticker_symbol VARCHAR(255),
-    shares NUMERIC(12, 4) NOT NULL
+    name VARCHAR(255) NOT NULL,
+    ticker_symbol VARCHAR(6),
+    shares NUMERIC(12, 4) NOT NULL,
+    UNIQUE (account_uuid, ticker_symbol)
 );
