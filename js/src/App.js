8782c6d03a65aadee8a7b40e1c768be557a2152d
DeclanCodes <DeclanCodes@users.noreply.github.com>
2020-05-06 15:23:34 -0400

    add first react component: - Add basic component for Account

--- js/src/App.js
+++ js/src/App.js
@@ -1,15 +1,18 @@
-import React, { Component} from "react";
+import React, { Component } from "react";
 import {hot} from "react-hot-loader"
 import axios from "axios"
+import Account from "./components/Account";
 import "./App.css";
+
 
 const API_URL = "http://localhost:8080"
 
-class App extends Component{
+class App extends Component {
   state = {
     accounts: []
   }
-  componentDidMount(){
+
+  componentDidMount() {
     const url = `${API_URL}/accounts`
     axios.get(url).then(response => response.data)
     .then((data) => {
@@ -17,25 +20,15 @@ class App extends Component{
       console.log(this.state.accounts)
     })
   }
-  render(){
-    return(
-      <div>
-        <div>
-          <h1>Finance Tracker</h1>
-          <h2>Accounts</h2>
-          {this.state.accounts.map((account) => (
-            <div className="account" key={account.accountUuid}>
-              <div className="account-body">
-                <h5>{account.name}</h5>
-                <h6>{account.accountCategory.name}</h6>
-                <h6>{account.description}</h6>
-                <h6>Amount: ${account.amount}</h6>
-              </div>
-            </div>
-          ))}
-        </div>
+
+  render() {
+    return (
+      <div className="accounts">
+        {this.state.accounts.map(account =>
+          (<Account account={account} key={account.accountUuid}/>)
+        )}
       </div>
-   );
+    );
   }
 }
 
