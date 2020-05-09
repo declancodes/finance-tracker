81ad37c5fcff9cf0ec6a1146dba0dc598262f36b
DeclanCodes <DeclanCodes@users.noreply.github.com>
2020-05-09 18:17:49 -0400

    add react-router-dom: - Add react-router-dom to better separate App component pages - Move AccountCategory, ExpenseCategory, and Account pages to Router within App

--- js/src/App.js
+++ js/src/App.js
@@ -1,5 +1,11 @@
 import React from "react";
-import {hot} from "react-hot-loader"
+import { hot } from "react-hot-loader";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import AccountsPage from "./components/accounts/AccountsPage";
 import CategoryPage from "./components/categories/CategoryPage";
 import "./App.css";
@@ -7,11 +13,34 @@ import "./App.css";
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <CategoryPage categoryType="Account"/>
-        <CategoryPage categoryType="Expense"/>
-        <AccountsPage/>
-      </div>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/accounts">Accounts</Link>
+              </li>
+              <li>
+                <Link to="/accountcategories">Account Categories</Link>
+              </li>
+              <li>
+                <Link to="/expensecategories">Expense Categories</Link>
+              </li>
+            </ul>
+          </nav>
+          <Switch>
+            <Route path="/accountcategories">
+              <CategoryPage categoryType="Account"/>
+            </Route>
+            <Route path="/expensecategories">
+              <CategoryPage categoryType="Expense"/>
+            </Route>
+            <Route path="/accounts">
+              <AccountsPage/>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
