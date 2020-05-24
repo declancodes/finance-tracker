448faa37ed67334888595fe18b7e9552c1abc9d3
DeclanCodes <DeclanCodes@users.noreply.github.com>
2020-05-24 17:23:00 -0400

    replace double quotes with single quotes in App component

--- js/src/App.jsx
+++ js/src/App.jsx
@@ -1,44 +1,44 @@
-import React from "react";
-import { hot } from "react-hot-loader";
+import React from 'react';
+import { hot } from 'react-hot-loader';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Link
-} from "react-router-dom";
-import { AccountsPage } from "./components/pages/AccountsPage";
-import { CategoryPage } from "./components/pages/CategoryPage";
-import { ContributionsPage } from "./components/pages/ContributionsPage";
-import { ExpensesPage } from "./components/pages/ExpensesPage";
-import "./App.css";
+} from 'react-router-dom';
+import { AccountsPage } from './components/pages/AccountsPage';
+import { CategoryPage } from './components/pages/CategoryPage';
+import { ContributionsPage } from './components/pages/ContributionsPage';
+import { ExpensesPage } from './components/pages/ExpensesPage';
+import './App.css';
 
 const App = () => (
   <Router>
     <div>
       <nav>
         <ul>
-          <li><Link to="/accounts">Accounts</Link></li>
-          <li><Link to="/accountcategories">Account Categories</Link></li>
-          <li><Link to="/contributions">Contributions</Link></li>
-          <li><Link to="/expenses">Expenses</Link></li>
-          <li><Link to="/expensecategories">Expense Categories</Link></li>
+          <li><Link to='/accounts'>Accounts</Link></li>
+          <li><Link to='/accountcategories'>Account Categories</Link></li>
+          <li><Link to='/contributions'>Contributions</Link></li>
+          <li><Link to='/expenses'>Expenses</Link></li>
+          <li><Link to='/expensecategories'>Expense Categories</Link></li>
         </ul>
       </nav>
       <Switch>
-        <Route path="/accounts">
+        <Route path='/accounts'>
           <AccountsPage/>
         </Route>
-        <Route path="/accountcategories">
-          <CategoryPage key="1" categoryType="Account"/>
+        <Route path='/accountcategories'>
+          <CategoryPage key='1' categoryType='Account'/>
         </Route>
-        <Route path="/contributions">
+        <Route path='/contributions'>
           <ContributionsPage/>
         </Route>
-        <Route path="/expenses">
+        <Route path='/expenses'>
           <ExpensesPage/>
         </Route>
-        <Route path="/expensecategories">
-          <CategoryPage key="2" categoryType="Expense"/>
+        <Route path='/expensecategories'>
+          <CategoryPage key='2' categoryType='Expense'/>
         </Route>
       </Switch>
     </div>
