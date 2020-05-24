78ef4a197658ef64c849f5b5edc8028e61318f9f
DeclanCodes <DeclanCodes@users.noreply.github.com>
2020-05-24 14:46:25 -0400

    convert App to function component

--- js/src/App.jsx
+++ js/src/App.jsx
@@ -12,51 +12,37 @@ import { ContributionsPage } from "./components/pages/ContributionsPage";
 import { ExpensesPage } from "./components/pages/ExpensesPage";
 import "./App.css";
 
-class App extends React.Component {
-  render() {
-    return (
-      <Router>
-        <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/accounts">Accounts</Link>
-              </li>
-              <li>
-                <Link to="/accountcategories">Account Categories</Link>
-              </li>
-              <li>
-                <Link to="/contributions">Contributions</Link>
-              </li>
-              <li>
-                <Link to="/expenses">Expenses</Link>
-              </li>
-              <li>
-                <Link to="/expensecategories">Expense Categories</Link>
-              </li>
-            </ul>
-          </nav>
-          <Switch>
-            <Route path="/accounts">
-              <AccountsPage/>
-            </Route>
-            <Route path="/accountcategories">
-              <CategoryPage key="1" categoryType="Account"/>
-            </Route>
-            <Route path="/contributions">
-              <ContributionsPage/>
-            </Route>
-            <Route path="/expenses">
-              <ExpensesPage/>
-            </Route>
-            <Route path="/expensecategories">
-              <CategoryPage key="2" categoryType="Expense"/>
-            </Route>
-          </Switch>
-        </div>
-      </Router>
-    );
-  }
-}
+const App = () => (
+  <Router>
+    <div>
+      <nav>
+        <ul>
+          <li><Link to="/accounts">Accounts</Link></li>
+          <li><Link to="/accountcategories">Account Categories</Link></li>
+          <li><Link to="/contributions">Contributions</Link></li>
+          <li><Link to="/expenses">Expenses</Link></li>
+          <li><Link to="/expensecategories">Expense Categories</Link></li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route path="/accounts">
+          <AccountsPage/>
+        </Route>
+        <Route path="/accountcategories">
+          <CategoryPage key="1" categoryType="Account"/>
+        </Route>
+        <Route path="/contributions">
+          <ContributionsPage/>
+        </Route>
+        <Route path="/expenses">
+          <ExpensesPage/>
+        </Route>
+        <Route path="/expensecategories">
+          <CategoryPage key="2" categoryType="Expense"/>
+        </Route>
+      </Switch>
+    </div>
+  </Router>
+);
 
 export default hot(module)(App);
