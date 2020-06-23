6b61df6ed128152165e0cc1b3ac9c49d5d29b720
DeclanCodes <DeclanCodes@users.noreply.github.com>
2020-06-23 10:25:45 -0400

    fix bug setting react-select initial value with Formik

--- js/src/components/forms/fields/LabeledSelectField.jsx
+++ js/src/components/forms/fields/LabeledSelectField.jsx
@@ -6,7 +6,7 @@ import { DarklyReactSelect } from '../../common/DarklyReactSelect';
 export const LabeledSelectField = ({ ...props }) => {
   const { setFieldValue } = useFormikContext();
   const [field] = useField(props);
-  const opts = () => helpers
+  const opts = helpers
     .getOptionsArrayFromKey(props.options, props.name)
     .map(o => {
       return {
@@ -14,12 +14,15 @@ export const LabeledSelectField = ({ ...props }) => {
         label: o[props.optionDisplay]
       }
     });
+  const selected = opts.length > 0 ?
+    opts.filter(o => o.value === field.value)[0] :
+    [];
 
   return (
     <DarklyReactSelect
       placeholder={`Select ${props.displayName}...`}
-      options={opts()}
-      value={field.value}
+      options={opts}
+      value={selected}
       onChange={value => setFieldValue(field.name, value)}
     />
   );
